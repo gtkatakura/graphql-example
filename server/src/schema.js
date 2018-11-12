23a563b320cbfec24ae3b710f5f8a7619861b7f0
gtkatakura <gt.katakura@gmail.com>
2018-11-12 01:54:51 -0200

    feat: Query#animes

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,36 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql'
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList,
+} from 'graphql'
+
+const AnimeType = new GraphQLObjectType({
+  name: 'Anime',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    episodes: { type: GraphQLFloat },
+    meanScore: { type: GraphQLFloat },
+  }
+})
+
+const animes = [
+  {
+    id: 1,
+    name: 'Mahoutsukai no Yome',
+    episodes: 24,
+    meanScore: 78,
+  },
+  {
+    id: 2,
+    name: 'Steins;Gate 0',
+    episodes: 23,
+    meanScore: 84,
+  }
+]
 
 export const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -9,6 +41,12 @@ export const schema = new GraphQLSchema({
         description: 'A simple type for getting started!',
         resolve: () => 'world',
       },
+      animes: {
+        type: new GraphQLList(AnimeType),
+        resolve: (_object, _args, _context) => {
+          return animes
+        }
+      }
     }
   }),
 })
