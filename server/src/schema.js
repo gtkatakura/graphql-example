2fa90b4ce6a23746058a1bb008f0872feb8c92d2
gtkatakura <gt.katakura@gmail.com>
2018-11-12 01:54:51 -0200

    feat: Query#animes

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,13 +4,39 @@ const typeDefs = `
   type Query {
     """A simple type for getting started!"""
     hello: String
+    animes: [Anime]!
+  }
+
+  type Anime {
+    id: ID
+    name: String
+    episodes: Float
+    meanScore: Float
   }
 `
+
+const animes = [
+  {
+    id: 1,
+    name: 'Mahoutsukai no Yome',
+    episodes: 24,
+    meanScore: 78,
+  },
+  {
+    id: 2,
+    name: 'Steins;Gate 0',
+    episodes: 23,
+    meanScore: 84,
+  }
+]
 
 const resolvers = {
   Query: {
     hello: (_root, _args, _context) => {
       return 'world'
+    },
+    animes: (_root, args, _context) => {
+      return animes
     },
   },
 }
